6f5e9676d3b0636f90785d972dd400dd028728d6
Abdul Muizz <163648314+Muizzgitfr@users.noreply.github.com>
2024-09-20 00:10:10 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+//CHAPTER NO 1
+// let name = "Abdul Muizz";
+// alert("Hello, " + name + "! Welcome to my website.");
+
+
+//CHAPTER NO 2
+// let favoriteBook = "Holy Quran";
+// document.write("My favorite book is: " + favoriteBook);
+
+
+//CHAPTER NO 3
+// let age = 20;
+// document.write("I am " + age + " years old.");
+
+
+//CHAPTER NO 4
+// Variable Names: Legal and Illegal
+
+// Legal variable names
+// var firstName = "Abdul";
+// var $amount = 2500;
+// var _status = "Active";
+// var user123 = "Muizz";
+// console.log(firstName);
+// console.log($amount);    
+// console.log(_status);
+// console.log(user123); 
+
+// Illegal variable names
+// var 1stPlace = "Not allowed";
+// var user-name = "Invalid";
+// var my name = "Error";
+// var #hashtag = "Nope";
+// Console log to show an example of a legal name
+// console.log("Variable names can start with letters, $, or _ but not with numbers or special characters.");
+
+
+//CHAPTER NO 5
+// let length = 5;
+// let width = 3;
+// let area = length * width;
+// document.write("The area of the rectangle is: " + area);
+
+
+//CHAPTER NO 6
+// let dividend = 17;
+// let divisor = 5;
+// let remainder = dividend % divisor;
+// document.write("The remainder of " + dividend + " divided by " + divisor + " is: " + remainder);
+
+
+//CHAPTER NO 7
+// let result = (2 + 3) * 4;
+// document.write("The result of the expression is: " + result);
+
+
+//CHAPTER NO 8
+// let greeting = "Hello, ";
+// let name = "Abdul Muizz";
+// let message = greeting + name + "! Welcome to my website.";
+// document.write(message);
+
+
+//CHAPTER NO 9
+// let name = prompt("What is your name?");
+// let age = parseInt(prompt("How old are you?"));
+// let message = "Hello, " + name + "! You are " + age + " years old.";
+// alert(message);
